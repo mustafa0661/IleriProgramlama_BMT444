795dcb92ba0a1728d26d41b38abb918c8e1fbc6e
Mustafa UZUN <mustafa.uzun1961ts@gmail.com>
2024-10-13 12:30:20 +0300

    Görünürlük eklendi

--- src/components/PropKullanimi.js
+++ src/components/PropKullanimi.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from "prop-types";
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
 export default class PropKullanimi extends Component {
     constructor(props){
@@ -7,6 +8,12 @@ export default class PropKullanimi extends Component {
         this.state = {
             isVisible : false
         }
+        //this.ilkEvent=this.ilkEvent.bind(this);
+    }
+    ilkEvent = (sayi, event) => {
+        this.setState({
+            isVisible : !this.state.isVisible
+        })
     }
   render() {
 
@@ -16,7 +23,7 @@ export default class PropKullanimi extends Component {
       <div className='col-md-7 mb-3'>
         <div className='card'>
             <div className='card-header d-flex justify-content-between'>
-                <h2 className='d-inline'>{bir}</h2>
+                <h2 className='d-inline' onClick={this.ilkEvent.bind(this.ilkEvent)}>{bir}</h2>
                 <i className='far fa-trash-alt' style={{cursor:'pointer'}}></i>
             </div>
             {isVisible ?
